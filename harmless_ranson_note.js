f46700eeccdbd2f4e5da43d6d39fbc53111dad18
Sergey Lebedev <oddyone@mail.ru>
2018-08-31 23:42:58 +0300

    Time and Big O Notation

---
+++ harmless_ranson_note.js
@@ -0,0 +1,57 @@
+// Big O Notation
+
+//Const runtime
+const log = (array) => {   // Big O Notation "0 (1)"
+    console.log([0]);
+    console.log([1]);
+};
+
+log([1, 2, 3]);
+log([1, 2, 3, 4]);
+
+// Linear runtime
+const logAll = (array) => {    // Big O Notation: "0 (n)"
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+    }
+};
+
+logAll([1, 2, 3, 4, 5]);
+logAll([1, 2, 3, 4, 5, 6]);
+logAll([1, 2, 3, 4, 5, 6, 7]);
+
+
+// Exponential runtime
+const addAndLog = (array) => {     // Big O Notation: "0 (n^2)"
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            console.log(array[i] + array[j]);
+        }
+    }
+};
+
+addAndLog(['A', 'B', 'C']);
+addAndLog(['A', 'B', 'C', 'D']);
+addAndLog(['A', 'B', 'C', 'D', 'E']);
+
+
+// Logarithmic runtime
+const binarySearch = (array, key) => {     // Bit O Notation: 0 (log n)
+    let low = 0;
+    let high = array.length - 1;
+    let mid;
+    let element;
+
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2, 10);
+        element = array[mid];
+        if (element < key) {
+            low = mid + 1;
+        } else if (element > key) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+};
