e670cbb1db0ecdb2b49b51a02f0582fa0040f7e9
Sergey Lebedev <oddyone@mail.ru>
2018-09-01 20:48:20 +0300

    Harmless Ransom Note - Part 2

--- harmless_ranson_note.js
+++ harmless_ranson_note.js
@@ -65,7 +65,16 @@ const harmlessRansomNote = (noteText, magazineText) => {
         if (!magazineObj[word]) magazineObj[word] = 0;
         magazineObj[word]++;
     });
-    console.log(magazineObj);
+
+    let noteIsPossible = true;
+    noteArr.forEach(word => {
+        if (magazineObj[word]) {
+            magazineObj--;
+            if (magazineObj[word] < 0) noteIsPossible = false;
+        }
+        else noteIsPossible = false;
+    });
+    return noteIsPossible;
 };
 
 harmlessRansomNote('', 'this is all the magazine text in the magazine');
